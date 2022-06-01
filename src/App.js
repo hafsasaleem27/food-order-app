8e0483407622d1badc44892a0223914e87977435
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-06-01 21:10:00 +0500

    Added Meals component, available meals component and mealssummary component and rendered meals component in the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <Meals/>
     </Fragment>
   );
 }
