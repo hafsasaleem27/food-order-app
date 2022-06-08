1a9fa820efcc117c82ab289124723dc9cf711d35
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-06-08 23:01:19 +0500

    Used cart context to calculate total amount

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onOpenCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{totalCartItems}</span>
     </button>
   );
 };
