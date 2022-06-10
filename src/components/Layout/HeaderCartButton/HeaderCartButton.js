47fb5f677c6322b97e5ee6a363e59047148c6a7a
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-06-11 02:36:25 +0500

    added visual tweak for showing cart numbers

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,16 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartIcon from "../../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const totalCartItems = cartCtx.items.reduce((currNum, item) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  const totalCartItems = items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
 
+  // useful piece of code for adding visual tweaks, notifications etc!
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onOpenCart}>
+    <button className={btnClasses} onClick={props.onOpenCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
