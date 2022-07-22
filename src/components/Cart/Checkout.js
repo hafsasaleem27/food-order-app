7e093af43bc9f434d9422e4fce0f972518a49d46
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-23 00:01:37 +0500

    wrote a preliminary submit form function

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -2,15 +2,19 @@ import React from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input id="name" type="text"/>
+        <input id="name" type="text" />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input id="street" type="text"/>
+        <input id="street" type="text" />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
@@ -20,7 +24,9 @@ const Checkout = (props) => {
         <label htmlFor="city">City</label>
         <input id="city" type="text" />
       </div>
-      <button type="button" onClick={props.onCancel}>Cancel</button>
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
       <button>Confirm</button>
     </form>
   );
