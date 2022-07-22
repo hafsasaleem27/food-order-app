fab21aafede0e190d965bf2f11d1f9f28af1c8e7
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-23 02:11:07 +0500

    added code for form input validations

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,28 +1,74 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isStreetValid, setIsStreetValid] = useState(true);
+  const [isPostalValid, setIsPostalValid] = useState(true);
+  const [isCityValid, setIsCityValid] = useState(true);
+
   const submitHandler = (event) => {
     event.preventDefault();
+    console.log("value in name input field: ", nameInputRef.current.value);
+    if (nameInputRef.current.value.trim() === "") setIsNameValid(false);
+    else setIsNameValid(true);
+    if (streetInputRef.current.value.trim() === "") setIsStreetValid(false);
+    else setIsStreetValid(true);
+    if (postalInputRef.current.value.trim() === "") setIsPostalValid(false);
+    else setIsPostalValid(true);
+    if (cityInputRef.current.value.trim() === "") setIsCityValid(false);
+    else setIsCityValid(true);
+
+    nameInputRef.current.value = "";
+    streetInputRef.current.value = "";
+    postalInputRef.current.value = "";
+    cityInputRef.current.value = "";
   };
+
+  const nameInputClasses = isNameValid
+    ? classes.control
+    : `${classes.control} ${classes.invalid}`;
+
+  const streetInputClasses = isStreetValid
+    ? classes.control
+    : `${classes.control} ${classes.invalid}`;
+
+  const postalInputClasses = isPostalValid
+    ? classes.control
+    : `${classes.control} ${classes.invalid}`;
+
+  const cityInputClasses = isCityValid
+    ? classes.control
+    : `${classes.control} ${classes.invalid}`;
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
-      <div className={classes.control}>
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" ref={nameInputRef} />
       </div>
-      <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input id="street" type="text" />
+      <div className={streetInputClasses}>
+        <label htmlFor="street" >
+          Street
+        </label>
+        <input id="street" type="text" ref={streetInputRef} />
       </div>
-      <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input id="postal" type="text" />
+      <div className={postalInputClasses}>
+        <label htmlFor="postal" >
+          Postal Code
+        </label>
+        <input id="postal" type="text" ref={postalInputRef}/>
       </div>
-      <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input id="city" type="text" />
+      <div className={cityInputClasses}>
+        <label htmlFor="city" >
+          City
+        </label>
+        <input id="city" type="text" ref={cityInputRef}/>
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
